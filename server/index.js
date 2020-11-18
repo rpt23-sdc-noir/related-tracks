aafca727fb00ca6f81dd418fc0576ba4ca92a780
Max Gektin <max.gektin@gmail.com>
2020-11-18 07:17:54 -0800

    Add full CRUD API

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const path = require('path');
 const chalk = require('chalk');
 const axios = require('axios');
-const db = require('../relatedData.js');
+const parser = require('body-parser');
+const morgan = require('morgan');
+const { addTrack, findTrack, updateTrack, deleteTrack, drop, close } = require('../relatedData.js');
 const expressStaticGzip = require('express-static-gzip');
 const app = express();
 
 const port = 3001;
+
+app.use(parser.json());
+app.use(morgan('dev'));
 
 // app.use(express.static(path.join(__dirname, '../public')));
 app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
@@ -17,60 +22,94 @@ app.use('/', expressStaticGzip(path.join(__dirname, '../public'), {
    }
 }))
 
-app.get('/relatedTracks/:song', (req, res) => {
-  // console.log(chalk.bgWhite.black(req.params.song));
-  db.findTrack(req.params.song, (err, data) => {
+// /relatedTracks/:song API
+app.post('/relatedTracks/:song', (req, res) => {
+  const track = {
+    'song_id': req.params.song,
+    'plays': req.body.plays,
+    'likes': req.body.likes,
+    'reposts': req.body.reposts,
+    'comments': req.body.comments,
+    'genre': req.body.genre,
+    'onPlaylists': req.body.onPlaylists,
+    'producedBy': req.body.producedBy,
+    'likedBy': req.body.likedBy,
+    'playedBy': req.body.playedBy,
+    'repostedBy': req.body.repostedBy,
+  };
+  addTrack(track, (err, data) => {
     if (err) {
-      console.log(chalk.red(`Problem obtaining track info: `, err));
-      res.status(500).send('There was a problem getting the track info');
+      console.log(`post err: ${err}`);
+      res.status(500).send('we had trouble adding that track; try again later');
     } else {
-      let relatedInfo = [];
-      if (data.related.length === 0) {
-        res.send(relatedInfo);
-      }
-      for (let i = 0; i < data.related.length; i++) {
-        let trackInfo = {};
-        trackInfo.song_id = data.related[i];
-        db.findTrack(data.related[i], (err, info) => {
-          if (err) {
-            console.log(chalk.red(`Can't find secondary track info: `, err));
-            res.status(500).send('Problem locating related track data');
-          } else {
-            trackInfo.plays = info.plays;
-            trackInfo.likes = info.likes;
-            trackInfo.reposts = info.reposts;
-            axios.get(`http://52.37.102.63:3005/songdata/${trackInfo.song_id}`)
-            .then(function (response) {
-              trackInfo.image = response.data.songImage;
-              trackInfo.song = response.data.songName;
-            })
-            .catch(function (error) {
-              console.log('ERROR GETTING IMAGE AND SONG NAME', error);
-              trackInfo.image = 'https://i1.sndcdn.com/avatars-000153260008-nj3jj1-t200x200.jpg';
-              trackInfo.song = 'Last Chance';
-            })
-            .then (function() {
-              axios.get(`http://34.220.154.45:2000/artistBio/${trackInfo.song_id}`)
-                .then(function (response) {
-                  trackInfo.band = response.data.data.bandName;
-                })
-                .catch(function (error) {
-                  console.log('ERROR GETTING BAND NAME', error);
-                  trackInfo.band = 'LionsBesideUs';
-                })
-                .then (function() {
-                  relatedInfo.push(trackInfo);
-                  if (relatedInfo.length === data.related.length) {
-                    res.send(relatedInfo);
-                  }
-                })
-            })
-          }
-        })
+      if (data === false) {
+        res.status(404).send('Track already exists');
+      } else {
+        res.status(200).send(`Track ${req.params.song} added`);
       }
     }
   })
-})
+});
+
+app.get('/relatedTracks/:song', (req, res) => {
+  findTrack(req.params.song, (err, data) => {
+    if (err) {
+      console.log(chalk.red(`Problem obtaining track info: ${err}`));
+      res.status(500).send('we had trouble finding that track; try again later');
+    } else {
+      if (data === null) {
+        res.status(404).send('No such track');
+      } else {
+        res.send(data);
+      }
+    }
+  });
+});
+
+app.put('/relatedTracks/:song', (req, res) => {
+  const replacement = {
+    'song_id': req.params.song,
+    'plays': req.body.plays,
+    'likes': req.body.likes,
+    'reposts': req.body.reposts,
+    'comments': req.body.comments,
+    'genre': req.body.genre,
+    'onPlaylists': req.body.onPlaylists,
+    'producedBy': req.body.producedBy,
+    'likedBy': req.body.likedBy,
+    'playedBy': req.body.playedBy,
+    'repostedBy': req.body.repostedBy,
+  };
+  updateTrack(req.params.song, replacement, (err, track) => {
+    if (err) {
+      console.log(`put error: ${err}`);
+      res.status(500).end('we had trouble updating that track; try again later');
+    } else {
+      if (track === null || !track.song_id) {
+        res.status(404).send('no such track');
+      } else {
+        console.log(`put data: ${track}`);
+        res.status(200).send(`track ${track.song_id} put on`);
+      }
+    }
+  });
+});
+
+app.delete('/relatedTracks/:song', (req, res) => {
+  let song = req.params.song;
+  deleteTrack(song, (err, track) => {
+    if (err) {
+      console.log(`delete error: ${err}`);
+      res.status(500).end('we had trouble deleting that track; try again later');
+    } else {
+      if (track === null) {
+        res.status(404).send('no such track');
+      } else {
+        res.status(200).send(`track ${track.song_id} deleted`);
+      }
+    }
+  });
+});
 
 app.get('/:current', (req, res) => {
   res.sendFile(path.join(__dirname,'../public/index.html'));
