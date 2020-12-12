f81543589a44d8faf6b0073c045cf602c95841e6
Ubuntu <ubuntu@ip-172-31-19-204.us-west-1.compute.internal>
2020-12-12 00:27:11 +0000

    Add postgres deploy

--- utils/generators.js
+++ utils/generators.js
@@ -1,11 +1,9 @@
-const faker = require('faker');
-
 const generateTrack = (id) => {
   return `${id + 1},${Math.ceil(Math.random() * 100000)},${Math.ceil(Math.random() * 1000000)}\n`;
 };
 
 const generateTrackUser = (id) => {
-  return `${id + 1},${Math.ceil(Math.random() * 10000000)},${Math.ceil(Math.random() * 5000000)}\n`;
+  return `${id + 1},${Math.ceil(Math.random() * 10000000)}\n`;
 };
 
 const generatePlaylistTrack = (id) => {
